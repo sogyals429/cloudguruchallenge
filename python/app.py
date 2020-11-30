9d4ee648d8ffa8bc8190ee01f028e589bc02096c
Sogyal Sherpa <sogyals429@gmail.com>
2020-11-30 22:06:40 +1100

    Code Transformation done

--- python/app.py
+++ python/app.py
@@ -3,8 +3,6 @@ import json
 import csv
 from datetime import datetime
 
-rawCases = []
-
 def fetchData(url):
   http = urllib3.PoolManager()
   request = http.request('GET', url)
@@ -14,34 +12,60 @@ def fetchData(url):
     raise Exception()
 
 def parseData(url):
+  rawCases = []
   try: 
     data = fetchData(url)
     reader = csv.DictReader(data.split("\n"), delimiter=',')
     for row in reader:
       rawCases.append(row)
+    return rawCases
   except Exception as e:
     print("Failed to parse data from url " + str(e))
 
-def cleanData():
+def cleanData(rawCases):
   try:
     cleanedData = parseDate(rawCases)
+    return cleanedData
   except Exception as e: 
     print("Exception occured while formatting data " + str(e))
 
 def parseDate(csvData):
-  cases = []
-  for line in csvData:
-    line["date"] = datetime.strptime(line["date"],'%Y-%m-%d').date()
-    cases.append(line)
-  return cases
+  try:
+    cases = []
+    for line in csvData:
+      if line.get('date'):
+        line["date"] = datetime.strptime(line["date"],'%Y-%m-%d').date()
+        cases.append(line)
+      elif line.get('Date'):
+        line["Date"] = datetime.strptime(line["Date"],'%Y-%m-%d').date()
+        cases.append(line)
+    return cases
+  except Exception as e:
+    print("Exception occured while parsing date " + str(e))
 
-def formatData():
+def formatData(originalCases):
   url = "https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv"
-  parseData(url)
+  cases = parseData(url)
+  usCases = cleanData(filterData(cases))
+
+  for case in originalCases:
+    for johnCase in usCases:
+      if case["date"] == johnCase["Date"]:
+        case["Recovered"] = johnCase["Recovered"]
+        print(case)
+  
+
+def filterData(cases):
+  rawCases = []
+  for case in cases:
+    if case["Country/Region"] == "US":
+      rawCases.append(case)
+  return rawCases
 
 if __name__ == "__main__":
   try :
-    parseData("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv")
-    cleanData()
+    nyTimesCases = parseData("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv")
+    cleanedDataNy = cleanData(nyTimesCases)
+    formatData(cleanedDataNy)
   except Exception as e:
-    print("An error occured while fetching data" + str(e))
+    print("An error occured while fetching data " + str(e))
